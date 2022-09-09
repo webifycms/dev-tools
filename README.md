31880ac1fd0084b8ee08abc9968129c045a4c921
Mohammed Shifreen <mshifreen@gmail.com>
2022-09-09 07:50:39 +0530

    Readme updated with intructions of installation and usage, default rules were updated and fixed formatting.

--- README.md
+++ README.md
@@ -2,6 +2,53 @@
 
 OneCMS PHP code sniffer tool to automatically fix PHP Coding Standards.
 
+## Installation
+
+First add the following to your `composer.json`
+
+```json
+...
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/getonecms/sniffer"
+  }
+]
+...
+```
+
+Now it can be install via composer
+
+```bash
+composer require getonecms/sniffer --dev
+```
+
+## Configuration
+
+You can add the rules and finder instance in the following way to your config file `.php-cs-fixer.php`:
+
+```php
+use OneCMS\Sniffer\Fixer;
+use PhpCsFixer\Finder;
+
+// create a finder instance according to your needs
+$finder = Finder::create()->in(__DIR__ . '/src');
+// add the rules and it will override the defaults
+$rules = [];
+
+return (new Fixer($finder, $rules))->getConfig();
+```
+
 ## Usage
 
 Run the following command to output the rules that can be applied.
+
+```bash
+composer sniff
+```
+
+Run the following command in order to format the files.
+
+```bash
+composer format
+```
--- src/Fixer.php
+++ src/Fixer.php
@@ -10,31 +10,22 @@ use PhpCsFixer\Finder;
 /**
  * The PHP Standard fixer class.
  *
- * @package getonecms/sniffer
  * @version 0.0.1
+ *
  * @since   0.0.1
+ *
  * @author  Mohammed Shifreen
  */
 final class Fixer
 {
-	/**
-	 * @var Finder
-	 */
 	private Finder $finder;
-	/**
-	 * @var array
-	 */
+
 	private array $rules;
-	/**
-	 * @var Config
-	 */
+
 	private Config $config;
 
 	/**
 	 * The class constructor.
-	 *
-	 * @param Finder $finder
-	 * @param array  $rules
 	 */
 	public function __construct(
 		Finder $finder,
@@ -47,29 +38,25 @@ final class Fixer
 			->setFinder($this->finder)
 			->setIndent("\t")
 			->setLineEnding("\n")
-			->setRiskyAllowed(true);
+			->setRiskyAllowed(true)
+		;
+	}
+
+	/**
+	 * Returns fixer configuration.
+	 */
+	public function getConfig(): Config
+	{
+		return $this->config;
 	}
 
 	/**
 	 * Merge the given rules with the default rules.
-	 *
-	 * @param  array $rules
-	 * @return array
 	 */
 	private function mergeRules(array $rules): array
 	{
 		$defaultRulesSet = require __DIR__ . '/rules.php';
 
 		return array_merge($defaultRulesSet, $rules);
-	}
-
-	/**
-	 * Returns fixer configuration.
-	 *
-	 * @return Config
-	 */
-	public function getConfig(): Config
-	{
-		return $this->config;
 	}
 }
