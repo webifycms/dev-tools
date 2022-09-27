451641cd8a0d103ffee7a99839ae67cc58baaf67
Mohammed Shifreen <mshifreen@gmail.com>
2022-09-27 09:41:29 +0530

    PHPCS Fixer updated, renamed src/rules.php to src/cs_fixer_rules.php and added .php-cs-fixer.php file in the root.

--- src/Fixer.php
+++ src/Fixer.php
@@ -1,27 +1,45 @@
 <?php
 
+/**
+ * The file is part of the "getonecms/dev-tools", OneCMS extension package.
+ *
+ * @see https://getonecms.com/extension/tools
+ *
+ * @copyright Copyright (c) 2022 OneCMS
+ * @license https://getonecms.com/extension/tools/license
+ * @author  Mohammed Shifreen <mshifreen@gmail.com>
+ */
+
 declare(strict_types=1);
 
-namespace OneCMS\Sniffer;
+namespace OneCMS\Tools;
 
 use PhpCsFixer\Config;
+use PhpCsFixer\ConfigInterface;
 use PhpCsFixer\Finder;
 
 /**
  * The PHP Standard fixer class.
  *
- * @author  Mohammed Shifreen
+ * @author Mohammed Shifreen
  */
 final class Fixer
 {
 	private Finder $finder;
 
+	/**
+	 * The rules.
+	 *
+	 * @var array<string>
+	 */
 	private array $rules;
 
-	private Config $config;
+	private ConfigInterface $config;
 
 	/**
 	 * The class constructor.
+	 *
+	 * @param array<string> $rules
 	 */
 	public function __construct(
 		Finder $finder,
@@ -41,17 +59,21 @@ final class Fixer
 	/**
 	 * Returns fixer configuration.
 	 */
-	public function getConfig(): Config
+	public function getConfig(): ConfigInterface
 	{
 		return $this->config;
 	}
 
 	/**
 	 * Merge the given rules with the default rules.
+	 *
+	 * @param array<string> $rules
+	 *
+	 * @return array<string>
 	 */
 	private function mergeRules(array $rules): array
 	{
-		$defaultRulesSet = require __DIR__ . '/rules.php';
+		$defaultRulesSet = require __DIR__ . '/cs_fixer_rules.php';
 
 		return array_merge($defaultRulesSet, $rules);
 	}
---
+++ src/cs_fixer_rules.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * The file is part of the "getonecms/dev-tools", OneCMS extension package.
+ *
+ * @see https://getonecms.com/extension/tools
+ *
+ * @copyright Copyright (c) 2022 OneCMS
+ * @license https://getonecms.com/extension/tools/license
+ * @author  Mohammed Shifreen <mshifreen@gmail.com>
+ */
+
+declare(strict_types=1);
+
+return [
+	'@PSR12'                  	      	=> true,
+	'@PSR12:risky'                 	 	=> true,
+	'@PHP80Migration:risky'        	 	=> true,
+	'@PHP81Migration'              	 	=> true,
+	'@PhpCsFixer'                  	 	=> true,
+	'@PhpCsFixer:risky'            	 	=> true,
+	'binary_operator_spaces'       	 	=> [ // overrides that inherits from @PhpCsFixer rule
+		'operators' => [
+			'=>' => 'align',
+			'='  => 'align',
+			'|'  => 'no_space',
+		],
+	],
+	'concat_space' => ['spacing' => 'one'], // overrides that inherits from @PhpCsFixer rule
+	'yoda_style'   => [
+		'equal'                => true,
+		'identical'            => true,
+		'less_and_greater'     => true,
+		'always_move_variable' => true,
+	],
+	// 'no_blank_lines_before_namespace' => true,
+];
