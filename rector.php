437bac3f35283ba83b01fceec1541e8c01826389
Mohammed Shifreen <mshifreen@gmail.com>
2023-05-05 15:27:19 +0530

    Rebranded and fix phpstan erros with updates of composer.json.

--- rector.php
+++ rector.php
@@ -1,21 +1,20 @@
 <?php
-
 /**
- * The file is part of the "getonecms/dev-tools", OneCMS extension package.
+ * The file is part of the "webifycms/dev-tools", WebifyCMS development tools.
  *
- * @see https://getonecms.com/extension/tools
+ * @see https://webifycms.com/tools/development-tools
  *
- * @copyright Copyright (c) 2022 OneCMS
- * @license https://getonecms.com/extension/tools/license
- * @author  Mohammed Shifreen <mshifreen@gmail.com>
+ * @license Copyright (c) 2022 WebifyCMS
+ * @license https://webifycms.com/extension/tools/license
+ * @author Mohammed Shifreen <mshifreen@gmail.com>
  */
 
 declare(strict_types=1);
 
-use OneCMS\Tools\Rector;
+use Webify\Tools\Rector;
 
 // Initialize
 return (new Rector())->initialize([
-    __DIR__ . '/src',
-    __DIR__ . '/test'
+    __DIR__.'/src',
+    __DIR__.'/test',
 ]);
--- src/Fixer.php
+++ src/Fixer.php
@@ -1,77 +1,77 @@
 <?php
 /**
- * The file is part of the "getonecms/dev-tools", OneCMS development tools.
+ * The file is part of the "webifycms/dev-tools", WebifyCMS development tools.
  *
- * @see https://getonecms.com/tools/development-tools
+ * @see https://webifycms.com/tools/development-tools
  *
- * @license Copyright (c) 2022 OneCMS
- * @license https://getonecms.com/extension/tools/license
+ * @license Copyright (c) 2022 WebifyCMS
+ * @license https://webifycms.com/extension/tools/license
  * @author Mohammed Shifreen <mshifreen@gmail.com>
  */
 
 declare(strict_types=1);
 
-namespace OneCMS\Tools;
+namespace Webify\Tools;
 
 use PhpCsFixer\Config;
 use PhpCsFixer\ConfigInterface;
 use PhpCsFixer\Finder;
+use function dirname;
 
 /**
  * The PHP Standard fixer class.
  */
 final class Fixer
 {
-	/**
-	 * The rules.
-	 *
-	 * @var array<string>
-	 */
-	private readonly array $rules;
+    /**
+     * The rules.
+     *
+     * @var array<string, array<string, mixed>|bool>
+     */
+    private readonly array $rules;
 
-	/**
-	 * The config object.
-	 */
-	private readonly ConfigInterface $config;
+    /**
+     * The config object.
+     */
+    private readonly ConfigInterface $config;
 
-	/**
-	 * The class constructor.
-	 *
-	 * @param array<string> $rules
-	 */
-	public function __construct(
-		public readonly Finder $finder,
-		array $rules = []
-	) {
-		$this->rules  = $this->mergeRules($rules);
-		$this->config = (new Config())
-			->setRules($this->rules)
-			->setFinder($this->finder)
-			->setIndent("\t")
-			->setLineEnding("\n")
-			->setRiskyAllowed(true)
-		;
-	}
+    /**
+     * The class constructor.
+     *
+     * @param array<string, array<string, mixed>|bool> $rules
+     */
+    public function __construct(
+        public readonly Finder $finder,
+        array                  $rules = []
+    ) {
+        $this->rules = $this->mergeRules($rules);
+        $this->config = (new Config())
+            ->setRules($this->rules)
+            ->setFinder($this->finder)
+            ->setIndent("\t")
+            ->setLineEnding("\n")
+            ->setRiskyAllowed(true);
+    }
 
-	/**
-	 * Returns fixer configuration.
-	 */
-	public function getConfig(): ConfigInterface
-	{
-		return $this->config;
-	}
+    /**
+     * Merge the given rules with the default rules.
+     *
+     * @param array<string, array<string, mixed>|bool> $rules
+     *
+     * @return array<string, array<string, mixed>|bool>
+     */
+    private function mergeRules(array $rules): array
+    {
+        $defaultRulesSet = require dirname(__DIR__) . '/.cs-default-rules.php';
 
-	/**
-	 * Merge the given rules with the default rules.
-	 *
-	 * @param array<string> $rules
-	 *
-	 * @return array<string>
-	 */
-	private function mergeRules(array $rules): array
-	{
-		$defaultRulesSet = require \dirname(__DIR__) . '/.cs-default-rules.php';
+        return array_merge($defaultRulesSet, $rules);
+    }
 
-		return array_merge($defaultRulesSet, $rules);
-	}
+    /**
+     * Returns fixer configuration.
+     */
+    public function getConfig(): ConfigInterface
+    {
+        return $this->config;
+    }
 }
